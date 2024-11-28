6afce0d3f197cdfb02c679ca8cea27f568f25b40
Daniel Roe <daniel@roe.dev>
2024-11-28 11:34:49 +0000

    test: add very basic test suite

--- playground/index.js
+++ playground/index.js
@@ -1,7 +1,11 @@
-import assert from 'node:assert'
-import * as pkg from 'oxc-walker'
+import { parseAndWalk } from 'oxc-walker'
+
+const nodes = []
+parseAndWalk('console.log("hello world")', 'test.js', {
+  enter(node) {
+    nodes.push(node)
+  },
+})
 
 // eslint-disable-next-line no-console
-console.log(pkg.parseAndWalk())
-
-assert.strictEqual(pkg.parseAndWalk(), 'hello world')
+console.log(nodes)
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,181 @@
-import { describe, it } from 'vitest'
+import type { Node } from '../src'
+import { describe, expect, it } from 'vitest'
+import { parseAndWalk } from '../src'
 
 describe('oxc-walker', () => {
-  it.todo('works', () => {
-    // expect().toMatchInlineSnapshot('"hello world"')
+  it('works', () => {
+    const nodes: Node[] = []
+    parseAndWalk('console.log("hello world")', 'test.js', {
+      enter(node) {
+        nodes.push(node)
+      },
+    })
+    expect(nodes).toMatchInlineSnapshot(`
+      [
+        {
+          "body": [
+            {
+              "end": 26,
+              "expression": {
+                "arguments": [
+                  {
+                    "end": 25,
+                    "raw": null,
+                    "start": 12,
+                    "type": "Literal",
+                    "value": "hello world",
+                  },
+                ],
+                "callee": {
+                  "end": 11,
+                  "object": {
+                    "end": 7,
+                    "name": "console",
+                    "start": 0,
+                    "type": "Identifier",
+                  },
+                  "optional": false,
+                  "property": {
+                    "end": 11,
+                    "name": "log",
+                    "start": 8,
+                    "type": "Identifier",
+                  },
+                  "start": 0,
+                  "type": "StaticMemberExpression",
+                },
+                "end": 26,
+                "optional": false,
+                "start": 0,
+                "type": "CallExpression",
+                "typeParameters": null,
+              },
+              "start": 0,
+              "type": "ExpressionStatement",
+            },
+          ],
+          "directives": [],
+          "end": 26,
+          "hashbang": null,
+          "sourceType": {
+            "language": "javascript",
+            "moduleKind": "module",
+            "variant": "jsx",
+          },
+          "start": 0,
+          "type": "Program",
+        },
+        {
+          "end": 26,
+          "expression": {
+            "arguments": [
+              {
+                "end": 25,
+                "raw": null,
+                "start": 12,
+                "type": "Literal",
+                "value": "hello world",
+              },
+            ],
+            "callee": {
+              "end": 11,
+              "object": {
+                "end": 7,
+                "name": "console",
+                "start": 0,
+                "type": "Identifier",
+              },
+              "optional": false,
+              "property": {
+                "end": 11,
+                "name": "log",
+                "start": 8,
+                "type": "Identifier",
+              },
+              "start": 0,
+              "type": "StaticMemberExpression",
+            },
+            "end": 26,
+            "optional": false,
+            "start": 0,
+            "type": "CallExpression",
+            "typeParameters": null,
+          },
+          "start": 0,
+          "type": "ExpressionStatement",
+        },
+        {
+          "arguments": [
+            {
+              "end": 25,
+              "raw": null,
+              "start": 12,
+              "type": "Literal",
+              "value": "hello world",
+            },
+          ],
+          "callee": {
+            "end": 11,
+            "object": {
+              "end": 7,
+              "name": "console",
+              "start": 0,
+              "type": "Identifier",
+            },
+            "optional": false,
+            "property": {
+              "end": 11,
+              "name": "log",
+              "start": 8,
+              "type": "Identifier",
+            },
+            "start": 0,
+            "type": "StaticMemberExpression",
+          },
+          "end": 26,
+          "optional": false,
+          "start": 0,
+          "type": "CallExpression",
+          "typeParameters": null,
+        },
+        {
+          "end": 11,
+          "object": {
+            "end": 7,
+            "name": "console",
+            "start": 0,
+            "type": "Identifier",
+          },
+          "optional": false,
+          "property": {
+            "end": 11,
+            "name": "log",
+            "start": 8,
+            "type": "Identifier",
+          },
+          "start": 0,
+          "type": "StaticMemberExpression",
+        },
+        {
+          "end": 7,
+          "name": "console",
+          "start": 0,
+          "type": "Identifier",
+        },
+        {
+          "end": 11,
+          "name": "log",
+          "start": 8,
+          "type": "Identifier",
+        },
+        {
+          "end": 25,
+          "raw": null,
+          "start": 12,
+          "type": "Literal",
+          "value": "hello world",
+        },
+      ]
+    `)
   })
 })
