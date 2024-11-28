8c118ffc9006ddd8894c584d63ac7f084d04fd64
Daniel Roe <daniel@roe.dev>
2024-11-28 12:31:11 +0000

    test: cover a few more cases

--- test/index.test.ts
+++ test/index.test.ts
@@ -7,9 +7,18 @@ describe('oxc-walker', () => {
     const nodes: Node[] = []
     parseAndWalk('console.log("hello world")', 'test.js', {
       enter(node) {
+        if (node.type !== 'Program') {
+          this.skip()
+          return
+        }
+        nodes.push(node)
+      },
+      leave(node) {
         nodes.push(node)
       },
     })
+    const [first, last] = nodes
+    expect(first).toStrictEqual(last)
     expect(nodes).toMatchInlineSnapshot(`
       [
         {
@@ -66,114 +75,125 @@ describe('oxc-walker', () => {
           "type": "Program",
         },
         {
-          "end": 26,
-          "expression": {
-            "arguments": [
-              {
-                "end": 25,
-                "raw": null,
-                "start": 12,
-                "type": "Literal",
-                "value": "hello world",
-              },
-            ],
-            "callee": {
-              "end": 11,
-              "object": {
-                "end": 7,
-                "name": "console",
+          "body": [
+            {
+              "end": 26,
+              "expression": {
+                "arguments": [
+                  {
+                    "end": 25,
+                    "raw": null,
+                    "start": 12,
+                    "type": "Literal",
+                    "value": "hello world",
+                  },
+                ],
+                "callee": {
+                  "end": 11,
+                  "object": {
+                    "end": 7,
+                    "name": "console",
+                    "start": 0,
+                    "type": "Identifier",
+                  },
+                  "optional": false,
+                  "property": {
+                    "end": 11,
+                    "name": "log",
+                    "start": 8,
+                    "type": "Identifier",
+                  },
+                  "start": 0,
+                  "type": "StaticMemberExpression",
+                },
+                "end": 26,
+                "optional": false,
                 "start": 0,
-                "type": "Identifier",
-              },
-              "optional": false,
-              "property": {
-                "end": 11,
-                "name": "log",
-                "start": 8,
-                "type": "Identifier",
+                "type": "CallExpression",
+                "typeParameters": null,
               },
               "start": 0,
-              "type": "StaticMemberExpression",
-            },
-            "end": 26,
-            "optional": false,
-            "start": 0,
-            "type": "CallExpression",
-            "typeParameters": null,
-          },
-          "start": 0,
-          "type": "ExpressionStatement",
-        },
-        {
-          "arguments": [
-            {
-              "end": 25,
-              "raw": null,
-              "start": 12,
-              "type": "Literal",
-              "value": "hello world",
+              "type": "ExpressionStatement",
             },
           ],
-          "callee": {
-            "end": 11,
-            "object": {
-              "end": 7,
-              "name": "console",
-              "start": 0,
-              "type": "Identifier",
-            },
-            "optional": false,
-            "property": {
-              "end": 11,
-              "name": "log",
-              "start": 8,
-              "type": "Identifier",
-            },
-            "start": 0,
-            "type": "StaticMemberExpression",
-          },
+          "directives": [],
           "end": 26,
-          "optional": false,
-          "start": 0,
-          "type": "CallExpression",
-          "typeParameters": null,
-        },
-        {
-          "end": 11,
-          "object": {
-            "end": 7,
-            "name": "console",
-            "start": 0,
-            "type": "Identifier",
-          },
-          "optional": false,
-          "property": {
-            "end": 11,
-            "name": "log",
-            "start": 8,
-            "type": "Identifier",
+          "hashbang": null,
+          "sourceType": {
+            "language": "javascript",
+            "moduleKind": "module",
+            "variant": "jsx",
           },
           "start": 0,
-          "type": "StaticMemberExpression",
+          "type": "Program",
         },
+      ]
+    `)
+  })
+
+  it('handles simple enter callback', () => {
+    const nodes: Node[] = []
+    parseAndWalk('console.log("hello world")', 'test.js', function (node) {
+      if (node.type !== 'Program') {
+        this.skip()
+        return
+      }
+      nodes.push(node)
+    })
+    expect(nodes).toMatchInlineSnapshot(`
+      [
         {
-          "end": 7,
-          "name": "console",
+          "body": [
+            {
+              "end": 26,
+              "expression": {
+                "arguments": [
+                  {
+                    "end": 25,
+                    "raw": null,
+                    "start": 12,
+                    "type": "Literal",
+                    "value": "hello world",
+                  },
+                ],
+                "callee": {
+                  "end": 11,
+                  "object": {
+                    "end": 7,
+                    "name": "console",
+                    "start": 0,
+                    "type": "Identifier",
+                  },
+                  "optional": false,
+                  "property": {
+                    "end": 11,
+                    "name": "log",
+                    "start": 8,
+                    "type": "Identifier",
+                  },
+                  "start": 0,
+                  "type": "StaticMemberExpression",
+                },
+                "end": 26,
+                "optional": false,
+                "start": 0,
+                "type": "CallExpression",
+                "typeParameters": null,
+              },
+              "start": 0,
+              "type": "ExpressionStatement",
+            },
+          ],
+          "directives": [],
+          "end": 26,
+          "hashbang": null,
+          "sourceType": {
+            "language": "javascript",
+            "moduleKind": "module",
+            "variant": "jsx",
+          },
           "start": 0,
-          "type": "Identifier",
-        },
-        {
-          "end": 11,
-          "name": "log",
-          "start": 8,
-          "type": "Identifier",
-        },
-        {
-          "end": 25,
-          "raw": null,
-          "start": 12,
-          "type": "Literal",
-          "value": "hello world",
+          "type": "Program",
         },
       ]
     `)
